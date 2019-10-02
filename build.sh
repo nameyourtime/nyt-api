a1c521f4980684fcf9034fe3fcfff5b520197413
alvox <al@alvox.net>
2019-10-02 19:25:33 -0400

    Use github docker registry.

--- build.sh
+++ build.sh
@@ -2,6 +2,9 @@
 
 rm -f app
 CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o app ./cmd/web/*
-docker build -t nyt-api:latest .
-#docker push ...
+docker build -t docker.pkg.github.com/nameyourtime/nyt-api/nyt-api:latest .
+docker push docker.pkg.github.com/nameyourtime/nyt-api/nyt-api:latest
 rm -f app
+
+# remove previous image(s)
+docker rmi $(docker images --filter dangling=true -q)
